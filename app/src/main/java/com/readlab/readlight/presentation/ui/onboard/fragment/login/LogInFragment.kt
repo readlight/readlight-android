81d2318883ac3b7f4587b78717169e66d26f9303
wkdwhddn06 <wkdwhddn06@naver.com>
2020-09-04 13:41:02 +0900

    feat: Change LogInActivity to Fragment

---
+++ app/src/main/java/com/readlab/readlight/presentation/ui/onboard/fragment/login/LogInFragment.kt
@@ -0,0 +1,31 @@
+package com.readlab.readlight.presentation.ui.onboard.fragment.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.readlab.readlight.databinding.FragmentLoginBinding
+import com.readlab.readlight.presentation.common.BaseFragment
+import com.readlab.readlight.presentation.ui.main.MainActivity
+import org.koin.android.ext.android.inject
+
+class LogInFragment : BaseFragment() {
+    private val logInViewModel: LogInViewModel by inject()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.vm = logInViewModel
+
+        binding.logInButton.setOnClickListener {
+            startActivity(Intent(activity, MainActivity::class.java))
+            activity?.finish()
+        }
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/readlab/readlight/presentation/ui/onboard/fragment/start/StartFragment.kt
+++ app/src/main/java/com/readlab/readlight/presentation/ui/onboard/fragment/start/StartFragment.kt
@@ -1,16 +1,18 @@
 package com.readlab.readlight.presentation.ui.onboard.fragment.start
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.rx
 import com.readlab.readlight.databinding.FragmentStartBinding
 import com.readlab.readlight.presentation.common.BaseFragment
+import com.readlab.readlight.presentation.ui.main.MainActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
-
 
 class StartFragment : BaseFragment() {
     private lateinit var binding: FragmentStartBinding
@@ -28,8 +30,8 @@ class StartFragment : BaseFragment() {
     fun kakaoLogin() = LoginClient.rx.loginWithKakaoTalk(parent)
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe({
-            //startActivity(Intent(this, MainActivity::class.java))
-            //finish()
+            startActivity(Intent(activity, MainActivity::class.java))
+            activity?.finish()
         }, { error ->
             Log.e("StartFragment", "로그인 실패", error)
         })
@@ -39,6 +41,7 @@ class StartFragment : BaseFragment() {
     }
 
     fun loginWithEmail() {
-        //startActivity(Intent(this, LogInActivity::class.java))
+        val direction = StartFragmentDirections.actionStartFragmentToLoginFragment()
+        findNavController().navigate(direction)
     }
 }
